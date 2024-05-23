0819703e52375af4cdf0046cce11b68dea516e85
dark1104 <thedarkghoul@gmail.com>
2024-05-23 11:09:51 +0000

    addition

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int Number1;
+	int Number2;
+	int Sum;
+
+	printf("First Number=");
+	scanf("%d",&Number1);
+	printf("Second Number=");
+	scanf("%d" , &Number2);
+
+	printf("Sum%d=", Number1 + Number2);
+	return 0;
+}
+
